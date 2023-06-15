c245d40e7c039b9d700c9b86a6dad95e79a5e410
Yu Wu <yw3748@columbia.edu>
2023-06-14 22:03:03 -0400

    lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,22 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function BioDisplay(props) {
+  const isBio = props.bio;
+  if (isBio) {
+    return <p>{props.bio}</p>
+  } else {
+    return null
+  }
+}
+
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <BioDisplay bio={props.bio} />
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.github} linkedin={props.linkedin} />
     </div>
   );
 }
